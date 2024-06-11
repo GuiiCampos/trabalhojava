54a851867fbfd0b153dc29918514e39f6d58d8bd
Gui <guiicampos31@gmail.com>
2024-06-11 12:17:59 -0300

    teste2

---
+++ src/DadosFuncionarios.java
@@ -0,0 +1,8 @@
+public class DadosFuncionarios {
+    String nome;
+    int idade;
+    String Alergias;
+    String Problemas_médicos;
+    int Telefone;
+    String E_mail;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int idade = 2;
-        System.out.println("Idade: " + idade);
+
     }
 }
